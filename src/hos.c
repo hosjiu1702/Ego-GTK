c06685468c886073b0d54f3102a2a82fbe80916a
hosjiu1702 <hosjiu1702@gmail.com>
2017-05-05 10:39:51 +0700

    b

--- src/hos.c
+++ src/hos.c
@@ -1,18 +1,34 @@
-#include <wiringSerial.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
+#include <wiringPi.h>
+#include <wiringSerial.h>
 int main()
 {
-	wiringPiSetup();
 
+	while(1)
+	{
 	int serial_port;
+
+  if (wiringPiSetup () == -1)
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
+}
+
 	if((serial_port = serialOpen("/dev/ttyAMA0", 9600)) < 0)
 	{
 		printf("\nerror during init serial");
 		return 0;
 	}
 	
-	serialPutchar(serial_port, 1);
+	for(int i=0; i<10; i++)
+	{
+		serialPutchar(serial_port, 1);
+	}
 
-	while(1){}
+	}
+
 	return 0;
 }
