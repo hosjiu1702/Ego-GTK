81cf7098db9699c203f7ed649b8db7d66c472f44
hosjiu <hosjiu1702@gmail.com>
2017-05-05 18:40:25 +0700

    c

--- src/hos.c
+++ src/hos.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 
 		//serialPutchar(serial_port, c);
 
-	write(serial_port, "h", 1);
+	write(serial_port, "h", 10);
 	//write(serial_port, "h", 1);
 		//delay(500);
 
